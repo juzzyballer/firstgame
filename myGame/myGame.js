22ba6d08afd8e8b181fde3514e139814ce9fc045
justin <jucasillas.de@mysummitps.org>
2016-10-14 16:35:02 +0000

    first commit

---
+++ myGame/myGame.js
@@ -0,0 +1,25 @@
+/*global Phaser*/
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+
+game.load.image('sky', 'assets/sky.png')
+game.load.image('ground', 'assets/platform.png')
+game.load.image('star', 'assets/star.png')
+game.load.spritesheet('dude', 'assets/dude.png',32,48)
+
+}
+
+function create() {
+
+game.add.sprite(0.0, 'star')
+game.add.sprite(0.0, 'sky')
+
+}
+
+function update() {
+
+}
+
+//y u do dis
